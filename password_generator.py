440f8cf11ffa2e9af9ab3834ca8c75fe924059d8
aloneanderson <aloneanderson@gmail.com>
2021-06-30 11:28:06 +0300

    password gen 1.0

---
+++ password_generator.py
@@ -0,0 +1,17 @@
+import random, string
+
+a = string.punctuation
+b = string.ascii_letters
+c = string.digits
+d = a + b + c
+
+
+def gen():
+    while True:
+        print('* Enter a value no more than "94" *')
+        s = int(input('Required password length: '))
+        p = "".join(random.sample(d, s))
+        print('Password:', p)
+
+
+gen()
